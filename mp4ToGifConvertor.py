26bcdb8fd90d704135fd60fea64782df39aaf445
Navneet Singh Negi <33743031+navneetsn18@users.noreply.github.com>
2019-09-24 19:32:52 +0530

    Create mp4ToGifConvertor.py

---
+++ mp4ToGifConvertor.py
@@ -0,0 +1,20 @@
+#mp4 to gif convertor
+import imageio,os
+from tqdm.auto import tqdm
+
+def gifConvertor(inputPath):
+    targetFormat=".gif"
+    outputPath=os.path.splitext(inputPath)[0]+targetFormat
+    print(f'Converting {inputPath} to {outputPath}')
+    reader=imageio.get_reader(inputPath)
+    fps=reader.get_meta_data()['fps']
+    writer=imageio.get_writer(outputPath,fps=fps)
+    for frames in tqdm(reader,desc="Converting: "):
+        writer.append_data(frames)
+        #print(f'Frames{frames}')
+    print("\n Conversion Completed!")
+    writer.close()
+
+pathOfClip = input("\n Enter the path of the clip: ")
+clip=os.path.abspath('pathOfClip')
+gifConvertor(pathOfClip)
